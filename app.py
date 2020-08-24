60a5275f4c24afc06101ed5c1f26cdf114ff6def
hata_poly <mh.mundus@gmail.com>
2020-08-24 15:02:25 +0900

    8/24

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect
+from flask import render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/categories')
+def categories():
+    return render_template('categories.html')
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
